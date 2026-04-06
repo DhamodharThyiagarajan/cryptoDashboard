5a20d4b91e36fc1363095d1cf3e19e37633391da
DhamodharThyiagarajan <dhamodcruz1@gmail.com>
2026-04-06 15:03:32 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <header className="bg-white shadow-md p-6">
-        <h1 className="text-2xl font-serif mx-24">
+      <header className="bg-white sm:shadow-md p-4 md:p-6">
+        <h1 className="text-xl md:text-2xl font-serif mx-4 md:mx-8 lg:mx-24">
           <span className="bg-red-600 text-white px-2 py-1 rounded">AI</span>
           <span> maBetter</span>
         </h1>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,9 +3,9 @@ import Navbar from '../Components/Navbar'
 import Main from '../Components/Main'
 const Dashboard = () => {
   return (
-    <div className='mx-auto bg-[#f8fafc]'>
+    <div className='mx-auto bg-[#f8fafc] '>
       <Navbar/>
-      <div className='p-6'>
+      <div className='p-4 md:p-6'>
       <Main/>
       </div>
     </div>
